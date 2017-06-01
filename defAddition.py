a9fdc0f2ce766bfb81a58fcccefc0949018bf92d
Jae Yong Lee <lee984@purdue.edu>
2017-06-01 23:34:00 +0900

    201706012333

---
+++ defAddition.py
@@ -0,0 +1,6 @@
+def addition (a, b):
+    result = a + b
+    return result
+
+finalResult = addition(10, 8)
+print(finalResult)
---
+++ defTest.py
@@ -0,0 +1,11 @@
+def add_10(value):
+    result = value + 10
+    return result, 6
+
+n,v = add_10(5)
+
+
+
+print(n)
+print(v)
+
